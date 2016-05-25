f2c4907dd35cb1f243b13e7a57a615e9c24634c2
CaMnter <yuanyu.camnter@gmail.com>
2016-05-26 00:06:31 +0800

    add demo in samples module

--- samples/src/main/java/com/camnter/easyvolley/samples/MainActivity.java
+++ samples/src/main/java/com/camnter/easyvolley/samples/MainActivity.java
@@ -1,12 +1,114 @@
 package com.camnter.easyvolley.samples;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.camnter.easyvolley.hurl.RequestQueue;
+import com.camnter.easyvolley.hurl.VolleyError;
+import com.camnter.easyvolley.hurl.toolbox.Volley;
+import com.camnter.easyvolley.samples.request.GsonRequest;
+import com.google.gson.annotations.SerializedName;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mGetContentText;
+
 
     @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.mGetContentText = (TextView) this.findViewById(R.id.get_content_text);
+
+        this.initData();
+    }
+
+
+    protected void initData() {
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        queue.add(new GsonRequest<GankData>("http://gank.io/api/data/Android/1/1", GankData.class) {
+            /**
+             * Called when a response is received.
+             */
+            @Override public void onResponse(GankData response) {
+                mGetContentText.setText(response.toString());
+            }
+
+
+            /**
+             * Callback method that an error has been occurred with the
+             * provided error code and optional user-readable message.
+             */
+            @Override public void onErrorResponse(VolleyError error) {
+                Toast.makeText(MainActivity.this, error.getMessage(), Toast.LENGTH_LONG).show();
+                Log.d("GsonRequest", error.getMessage());
+            }
+        });
+    }
+
+
+    public class GankData {
+        private static final String TAG = "GankData";
+
+        public boolean error;
+        public ArrayList<GankResultData> results;
+
+
+        @Override public String toString() {
+            StringBuilder builder = new StringBuilder(TAG).append("\n\n");
+            for (GankResultData result : results) {
+                builder.append(result.toString());
+                builder.append("\n\n");
+            }
+            return builder.toString();
+        }
+    }
+
+    public class GankResultData {
+
+        private static final String TAG = "GankResultData";
+
+        @SerializedName("_id") public String id;
+        public String createdAt;
+        public String desc;
+        public String publishedAt;
+        public String source;
+        public String type;
+        public String url;
+        public boolean used;
+        public String who;
+
+
+        @Override public String toString() {
+            return TAG + "id: " +
+                    this.id +
+                    "\n" +
+                    "createdAt: " +
+                    this.createdAt +
+                    "\n" +
+                    "desc: " +
+                    this.desc +
+                    "\n" +
+                    "publishedAt: " +
+                    this.publishedAt +
+                    "\n" +
+                    "source: " +
+                    this.source +
+                    "\n" +
+                    "type: " +
+                    this.type +
+                    "\n" +
+                    "url: " +
+                    this.url +
+                    "\n" +
+                    "used: " +
+                    this.used +
+                    "\n" +
+                    "who: " +
+                    this.who;
+        }
     }
 }
---
+++ samples/src/main/java/com/camnter/easyvolley/samples/request/GsonRequest.java
@@ -0,0 +1,86 @@
+package com.camnter.easyvolley.samples.request;
+
+import com.camnter.easyvolley.hurl.NetworkResponse;
+import com.camnter.easyvolley.hurl.ParseError;
+import com.camnter.easyvolley.hurl.Request;
+import com.camnter.easyvolley.hurl.Response;
+import com.camnter.easyvolley.hurl.VolleyError;
+import com.camnter.easyvolley.hurl.toolbox.HttpHeaderParser;
+import com.google.gson.Gson;
+import java.io.UnsupportedEncodingException;
+
+/**
+ * Description：GsonRequest
+ * Created by：CaMnter
+ * Time：2016-05-25 12:01
+ */
+public abstract class GsonRequest<T> extends Request<T>
+        implements Response.Listener<T>, Response.ErrorListener {
+
+    protected static final String PROTOCOL_CHARSET = "utf-8";
+
+    private Gson mGson;
+    private Response.Listener<T> mResponseListener;
+    private Class<T> mClass;
+
+
+    public GsonRequest(String url, Class<T> clazz) {
+        this(Method.GET, url, clazz);
+    }
+
+
+    public GsonRequest(int method, String url, Class<T> clazz) {
+        super(method, url, null);
+        this.mGson = new Gson();
+        this.mClass = clazz;
+        this.mResponseListener = this;
+    }
+
+
+    @Override protected Response<T> parseNetworkResponse(NetworkResponse response) {
+        try {
+            String jsonString = new String(response.data,
+                    HttpHeaderParser.parseCharset(response.headers, PROTOCOL_CHARSET));
+            return Response.success(this.mGson.fromJson(jsonString, this.mClass),
+                    HttpHeaderParser.parseCacheHeaders(response));
+        } catch (UnsupportedEncodingException e) {
+            return Response.error(new ParseError(e));
+        }
+    }
+
+
+    @Override protected void deliverResponse(T response) {
+        this.mResponseListener.onResponse(response);
+    }
+
+
+    /**
+     * @return this request's {@link com.camnter.easyvolley.hurl.Response.ErrorListener}.
+     */
+    public Response.ErrorListener getErrorListener() {
+        return this;
+    }
+
+
+    /**
+     * Delivers error message to the ErrorListener that the Request was
+     * initialized with.
+     *
+     * @param error Error details
+     */
+    @Override public void deliverError(VolleyError error) {
+        this.onErrorResponse(error);
+    }
+
+
+    /**
+     * Called when a response is received.
+     */
+    public abstract void onResponse(T response);
+
+    /**
+     * Callback method that an error has been occurred with the
+     * provided error code and optional user-readable message.
+     */
+    public abstract void onErrorResponse(VolleyError error);
+}
