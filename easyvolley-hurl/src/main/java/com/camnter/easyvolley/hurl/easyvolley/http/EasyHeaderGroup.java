56bdc1642dfb238ea7bed7da307247cfe8132afa
CaMnter <yuanyu.camnter@gmail.com>
2016-05-27 12:13:54 +0800

    set license

--- easyvolley-hurl/src/main/java/com/camnter/easyvolley/hurl/easyvolley/http/EasyHeaderGroup.java
+++ easyvolley-hurl/src/main/java/com/camnter/easyvolley/hurl/easyvolley/http/EasyHeaderGroup.java
@@ -1,32 +1,18 @@
 /*
- * $HeadURL: http://svn.apache.org/repos/asf/httpcomponents/httpcore/trunk/module-main/src/main/java/org/apache/http/message/HeaderGroup.java $
- * $Revision: 659185 $
- * $Date: 2008-05-22 11:07:36 -0700 (Thu, 22 May 2008) $
+ * Copyright (C) 2016 CaMnter yuanyu.camnter@gmail.com
+ * Copyright (C) 2008 The Apache Software Foundation (ASF)
  *
- * ====================================================================
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- * ====================================================================
- *
- * This software consists of voluntary contributions made by many
- * individuals on behalf of the Apache Software Foundation.  For more
- * information on the Apache Software Foundation, please see
- * <http://www.apache.org/>.
- *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package com.camnter.easyvolley.hurl.easyvolley.http;
--- easyvolley-hurl/src/main/java/com/camnter/easyvolley/hurl/easyvolley/http/EasyHeaderValueParser.java
+++ easyvolley-hurl/src/main/java/com/camnter/easyvolley/hurl/easyvolley/http/EasyHeaderValueParser.java
@@ -1,32 +1,18 @@
 /*
- * $HeadURL: http://svn.apache.org/repos/asf/httpcomponents/httpcore/trunk/module-main/src/main/java/org/apache/http/message/BasicHeaderValueParser.java $
- * $Revision: 595670 $
- * $Date: 2007-11-16 06:15:01 -0800 (Fri, 16 Nov 2007) $
+ * Copyright (C) 2016 CaMnter yuanyu.camnter@gmail.com
+ * Copyright (C) 2008 The Apache Software Foundation (ASF)
  *
- * ====================================================================
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- * ====================================================================
- *
- * This software consists of voluntary contributions made by many
- * individuals on behalf of the Apache Software Foundation.  For more
- * information on the Apache Software Foundation, please see
- * <http://www.apache.org/>.
- *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package com.camnter.easyvolley.hurl.easyvolley.http;
