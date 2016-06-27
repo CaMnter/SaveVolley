df523e60937373f4176c5493b768bb87f90492fb
CaMnter <yuanyu.camnter@gmail.com>
2016-06-27 15:35:40 +0800

    the main update about savevolley-hurl-fastjson module

---
+++ extensions/fastjson/savevolley-okhttp3-fastjson/src/main/java/com/camnter/savevolley/okhttp3/fastjson/request/OkHttp3FastjsonRequest.java
@@ -0,0 +1,88 @@
+package com.camnter.savevolley.okhttp3.fastjson.request;
+
+import com.alibaba.fastjson.JSON;
+import com.camnter.savevolley.okhttp3.volley.NetworkResponse;
+import com.camnter.savevolley.okhttp3.volley.ParseError;
+import com.camnter.savevolley.okhttp3.volley.Request;
+import com.camnter.savevolley.okhttp3.volley.Response;
+import com.camnter.savevolley.okhttp3.volley.VolleyError;
+import com.camnter.savevolley.okhttp3.volley.toolbox.HttpHeaderParser;
+import java.io.UnsupportedEncodingException;
+
+/**
+ * Description：OkHttp3FastjsonRequest
+ * Created by：CaMnter
+ * Time：2016-06-27 14:59
+ */
+
+public abstract class OkHttp3FastjsonRequest<T> extends Request<T>
+    implements Response.Listener<T>, Response.ErrorListener {
+
+    protected static final String PROTOCOL_CHARSET = "utf-8";
+
+    private Response.Listener<T> mResponseListener;
+    private Class<T> mClass;
+
+
+    public OkHttp3FastjsonRequest(String url, Class<T> clazz) {
+        this(Method.GET, url, clazz);
+    }
+
+
+    public OkHttp3FastjsonRequest(int method, String url, Class<T> clazz) {
+        super(method, url, null);
+        this.mClass = clazz;
+        this.mResponseListener = this;
+    }
+
+
+    @Override
+    protected Response<T> parseNetworkResponse(NetworkResponse response) {
+
+        try {
+            String jsonString = new String(response.data,
+                HttpHeaderParser.parseCharset(response.headers, PROTOCOL_CHARSET));
+            return Response.success(
+                JSON.parseObject(jsonString, this.mClass),
+                HttpHeaderParser.parseCacheHeaders(response));
+        } catch (UnsupportedEncodingException e) {
+            return Response.error(new ParseError(e));
+        }
+    }
+
+
+    @Override protected void deliverResponse(T response) {
+        this.mResponseListener.onResponse(response);
+    }
+
+
+    /**
+     * @return this request's {@link Response.ErrorListener}.
+     */
+    public Response.ErrorListener getErrorListener() {
+        return this;
+    }
+
+
+    /**
+     * Delivers error message to the ErrorListener that the Request was
+     * initialized with.
+     *
+     * @param error Error details
+     */
+    @Override public void deliverError(VolleyError error) {
+        this.onErrorResponse(error);
+    }
+
+
+    /**
+     * Called when a response is received.
+     */
+    public abstract void onResponse(T response);
+
+    /**
+     * Callback method that an error has been occurred with the
+     * provided error code and optional user-readable message.
+     */
+    public abstract void onErrorResponse(VolleyError error);
+}
